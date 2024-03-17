a48ab158f6a9e0b4d16fb52bf8e2016adfb1a755
Oleksandr Shynkarenko <57036990+AndrasteCries@users.noreply.github.com>
2024-03-17 14:02:28 +0200

    Rework parser

--- parser.rb
+++ parser.rb
@@ -12,13 +12,24 @@ doc = Nokogiri::HTML(driver.page_source)
 
 doc.css('ol[role="list"] li a').each_with_index do |element, i|
   link_text = element.text.strip
-  write_data << [i + 1, link_text]
+  link_text_id = "##{i + 1}-#{element.text.strip.downcase.gsub(' ', '-')}"
+
+  element_with_id = doc.css("#{link_text_id}")
+
+  image_element = element_with_id.css('img').first
+
+  image_src = image_element ? image_element['src'] : nil
+
+  text_content = element_with_id.css('p').text.strip
+
+  write_data << [i + 1, link_text, image_src, text_content]
+
 end
 
 driver.quit
 
 CSV.open("myfile.csv", "w") do |csv|
-  csv << ["ID", "Name"]
+  csv << ["ID", "Name", "Image Src", "Text Content"]
   write_data.each do |data|
     csv << data
   end
