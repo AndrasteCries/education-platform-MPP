b59e61d86b2d2d9cda045ce0c2c53b6582f97e7a
Oleksandr Shynkarenko <57036990+AndrasteCries@users.noreply.github.com>
2024-03-19 15:01:55 +0200

    Add teacher email validation (only 'karazin.ua' domen)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   def welcome
     @links = Link.all
-    # @teachers = Teacher.all
-    # @students = Student.all
+    @teachers = Teacher.all
+    @students = Student.all
     render 'pages/main'
   end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,18 @@
 class Teacher < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validate :valid_email_domain
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  private
+
+  def valid_email_domain
+    allowed_domains = ['karazin.ua']
+    email_domain = email.split('@').last if email.present?
+    unless allowed_domains.include?(email_domain)
+      errors.add(:email, "wrong domen")
+    end
+  end
+
 end
