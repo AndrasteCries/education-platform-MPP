b13e0ffdfba6bb92a3eba2d99d44ba7e83860cc1
Oleksandr Shynkarenko <57036990+AndrasteCries@users.noreply.github.com>
2024-04-14 14:26:04 +0300

    Refactor remove/add_students_form.rb 1. Replace script to them 2. Student search work

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,22 +3,3 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "channels"
-
-document.addEventListener('DOMContentLoaded', function() {
-    const searchInput = document.querySelector('.search-input');
-    const studentList = document.querySelector('.student-list');
-
-    searchInput.addEventListener('input', function() {
-        const searchValue = this.value.toLowerCase();
-        const students = studentList.querySelectorAll('label');
-
-        students.forEach(function(student) {
-            const fullName = student.textContent.toLowerCase();
-            if (fullName.includes(searchValue)) {
-                student.parentElement.style.display = 'block';
-            } else {
-                student.parentElement.style.display = 'none';
-            }
-        });
-    });
-});
